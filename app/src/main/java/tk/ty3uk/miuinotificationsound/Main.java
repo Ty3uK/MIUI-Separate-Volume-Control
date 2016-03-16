011da63e90a3f754bf2d5053fba8770340ef9a78
Maksim Karelov <max.karelov@gmail.com>
2016-03-17 02:21:02 +0300

    Version 1.4 released. Added support of Marshmallow MIUIv7 (android 6.x)

--- app/src/main/java/tk/ty3uk/miuinotificationsound/Main.java
+++ app/src/main/java/tk/ty3uk/miuinotificationsound/Main.java
@@ -17,21 +17,24 @@ import java.util.Arrays;
 public class Main implements IXposedHookLoadPackage {
     final static int STREAM_NOTIFICATION = 5;
 
-    final static String SETTINGS_CLASS = "com.android.settings";
+    final static String SETTINGS_PACKAGE = "com.android.settings";
     final static String SETTINGS_RINGER_VOLUME_FRAGMENT_CLASS = "com.android.settings.sound.RingerVolumeFragment";
     final static String SETTINGS_RINGER_VOLUME_FRAGMENT_METHOD = "onViewCreated";
 
     final static String AUDIO_SERVICE_CLASS = "android.media.AudioService";
+    final static String AUDIO_SERVICE_CLASS_MM = "com.android.server.audio.AudioService";
     final static String AUDIO_SERVICE_METHOD = "updateStreamVolumeAlias";
     final static String AUDIO_SERVICE_STREAM_VOLUME_ALIAS = "mStreamVolumeAlias";
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals(SETTINGS_CLASS)) {
-            XposedHelpers.findAndHookMethod(SETTINGS_RINGER_VOLUME_FRAGMENT_CLASS, lpparam.classLoader, SETTINGS_RINGER_VOLUME_FRAGMENT_METHOD, View.class, Bundle.class, new XC_MethodHook() {
+        if (lpparam.packageName.equals(SETTINGS_PACKAGE)) {
+            final Class<?> RingerVolumeFragment = XposedHelpers.findClass(SETTINGS_RINGER_VOLUME_FRAGMENT_CLASS, lpparam.classLoader);
+
+            XposedHelpers.findAndHookMethod(RingerVolumeFragment, SETTINGS_RINGER_VOLUME_FRAGMENT_METHOD, View.class, Bundle.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     View view = (View) param.args[0];
-                    int notificationSection = view.getResources().getIdentifier(SETTINGS_CLASS.concat(":id/notification_section"), null, null);
+                    int notificationSection = view.getResources().getIdentifier(SETTINGS_PACKAGE.concat(":id/notification_section"), null, null);
 
                     XposedBridge.log("notification_section id: " + notificationSection);
 
@@ -44,18 +47,37 @@ public class Main implements IXposedHookLoadPackage {
         }
 
         if (lpparam.packageName.equals("android")) {
-            XposedHelpers.findAndHookMethod(AUDIO_SERVICE_CLASS, lpparam.classLoader, AUDIO_SERVICE_METHOD, boolean.class, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    int[] mStreamVolumeAlias = (int[]) XposedHelpers.getObjectField(param.thisObject, AUDIO_SERVICE_STREAM_VOLUME_ALIAS);
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                final Class<?> AudioService = XposedHelpers.findClass(AUDIO_SERVICE_CLASS_MM, lpparam.classLoader);
 
-                    XposedBridge.log("mStreamVolumeAlias levels BEFORE: " + Arrays.toString(mStreamVolumeAlias));
+                XposedHelpers.findAndHookMethod(AudioService, AUDIO_SERVICE_METHOD, boolean.class, String.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        int[] mStreamVolumeAlias = (int[]) XposedHelpers.getObjectField(param.thisObject, AUDIO_SERVICE_STREAM_VOLUME_ALIAS);
 
-                    mStreamVolumeAlias[STREAM_NOTIFICATION] = STREAM_NOTIFICATION;
+                        XposedBridge.log("mStreamVolumeAlias levels BEFORE: " + Arrays.toString(mStreamVolumeAlias));
 
-                    XposedBridge.log("mStreamVolumeAlias levels after: " + Arrays.toString(mStreamVolumeAlias));
-                }
-            });
+                        mStreamVolumeAlias[STREAM_NOTIFICATION] = STREAM_NOTIFICATION;
+
+                        XposedBridge.log("mStreamVolumeAlias levels after: " + Arrays.toString(mStreamVolumeAlias));
+                    }
+                });
+            } else {
+                final Class<?> AudioService = XposedHelpers.findClass(AUDIO_SERVICE_CLASS, lpparam.classLoader);
+
+                XposedHelpers.findAndHookMethod(AudioService, AUDIO_SERVICE_METHOD, boolean.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        int[] mStreamVolumeAlias = (int[]) XposedHelpers.getObjectField(param.thisObject, AUDIO_SERVICE_STREAM_VOLUME_ALIAS);
+
+                        XposedBridge.log("mStreamVolumeAlias levels BEFORE: " + Arrays.toString(mStreamVolumeAlias));
+
+                        mStreamVolumeAlias[STREAM_NOTIFICATION] = STREAM_NOTIFICATION;
+
+                        XposedBridge.log("mStreamVolumeAlias levels after: " + Arrays.toString(mStreamVolumeAlias));
+                    }
+                });
+            }
         }
     }
 }
